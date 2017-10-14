54231426131c1c6773ff214ce581d69946cc67b1
kuina <kuinanein@gmail.com>
2017-10-14 14:45:33 +0900

    generated as Spring Starter Project

---
+++ src/main/java/kuina/spring_social_sbox/SpringSocialSboxApplication.java
@@ -0,0 +1,12 @@
+package kuina.spring_social_sbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialSboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialSboxApplication.class, args);
+	}
+}
---
+++ src/test/java/kuina/spring_social_sbox/SpringSocialSboxApplicationTests.java
@@ -0,0 +1,16 @@
+package kuina.spring_social_sbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSocialSboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
